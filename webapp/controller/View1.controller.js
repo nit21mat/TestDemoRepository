233eba8a86185bad1285af769ab3aae6f15523e8
Shourav Das <itsshourav@gmail.com>
2017-04-12 13:14:04 +0000

    Combo Box

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+		'jquery.sap.global',
+		'sap/ui/core/mvc/Controller',
+		'sap/ui/model/json/JSONModel'
+	], function(jQuery, Controller, JSONModel){
 	"use strict";
-
-	return Controller.extend("com.acc.trainingTestDemo.controller.View1", {
-
+	
+    return Controller.extend("com.acc.trainingTestDemo.controller.View1", { 
+     onInit: function () {
+ 			// set explored app's demo model on this sample
+			var oModel = new JSONModel(jQuery.sap.getModulePath("com.acc.trainingTestDemo.model", "/Status.json"));
+			this.getView().setModel(oModel);
+     }
 	});
 });
