e0824e361c309da7bb8f1170d279cf7796495e12
Nitin Bhagat <nit21mat@gmail.com>
2017-04-14 11:53:53 +0000

    view1.controller.js commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 		'jquery.sap.global',
 		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel'
-	], function(jQuery, Controller, JSONModel){
+		'sap/ui/model/json/JSONModel',
+		'sap/ui/model/Filter',
+	'sap/ui/model/FilterOperator'
+	], function(jQuery, Controller, JSONModel, Filter, FilterOperator) {
 	"use strict";
 	
     return Controller.extend("com.acc.trainingTestDemo.controller.View1", { 
@@ -10,6 +12,36 @@ sap.ui.define([
  			// set explored app's demo model on this sample
 			 /*var oModel = new JSONModel(jQuery.sap.getModulePath("com.acc.trainingTestDemo.model", "/Status.json"));
 			 this.getView().setModel(oModel,"status");*/
-     }
+			 this.oFilterBar = null;
+		var sViewId = this.getView().getId();
+ 
+		this.oFilterBar = sap.ui.getCore().byId(sViewId + "--filterBar");
+ 
+		this.oFilterBar.registerFetchData(this.fFetchData);
+		this.oFilterBar.registerApplyData(this.fApplyData);
+		this.oFilterBar.registerGetFiltersWithValues(this.fGetFiltersWithValues);
+ 
+		this.fVariantStub();
+ 
+		this.onToggleSearchField();
+ 
+		this.oFilterBar.fireInitialise();
+ 
+		this._sHeader = this.oFilterBar.getHeader();
+     },
+		onSearch : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.getView().byId("idProductsTable");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		}
 	});
 });
