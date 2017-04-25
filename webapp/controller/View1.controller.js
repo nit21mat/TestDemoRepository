dd4d36fb474a2461496faa484195f1a1d3276294
s0011952991trial <s0011952991trial>
2017-04-25 02:23:29 +0000

    Routing with Parameter Push

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -40,7 +40,7 @@ sap.ui.define([
 						oRouter.navTo("View2");*/
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("View2", {
-				soaId: oEvent.getSource().getBindingContext().getProperty("soaIdRes")
+				soaId: oEvent.getSource().getBindingContext().getProperty("SoaIdRes")
 			});
 		},
 
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,21 +1,26 @@
 sap.ui.define([
-		'jquery.sap.global',
-		'sap/ui/core/Fragment',
-		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel'
-	], function(jQuery, Fragment, Controller, JSONModel) {
+	'jquery.sap.global',
+	"sap/ui/core/UIComponent",
+	'sap/ui/core/Fragment',
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel'
+], function(jQuery, Fragment, Controller, JSONModel, UIComponent) {
 	"use strict";
- 
+
 	return Controller.extend("com.acc.trainingTestDemo.controller.View2", {
- 
+
 		onInit: function() {
+			this._getRouter()
+				.attachRouteMatched(this.onRouteMatched, this);
+		},
+
+		onExit: function() {
 			//Do Nothing
 		},
- 
-		onExit : function () {
-			//Do Nothing
+		_getRouter: function() {
+			return UIComponent.getRouterFor(this);
 		}
- 
+
 	});
- 
+
 });
