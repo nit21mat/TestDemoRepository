7f1ff88a5bdf145e95f05961268be9be00a6ea15
s0011952991trial <s0011952991trial>
2017-05-02 14:00:47 +0000

    Commit

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,43 +1,31 @@
 sap.ui.define([
-                'jquery.sap.global',
-                'sap/ui/core/mvc/Controller',
-                'sap/ui/core/UIComponent',
-                'sap/ui/core/Fragment',
-                'sap/ui/model/json/JSONModel'
+	'jquery.sap.global',
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/core/UIComponent',
+	'sap/ui/core/Fragment',
+	'sap/ui/model/json/JSONModel'
+
 ], function(jQuery, Controller, UIComponent, Fragment, JSONModel) {
-                "use strict";
-                var soaId;
-                return Controller.extend("com.acc.trainingTestDemo.controller.View2", {
+	"use strict";
+	return Controller.extend("com.acc.trainingTestDemo.controller.View2", {
 
-                                onInit: function() {
-                                                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                                                oRouter.getRoute("View2").attachPatternMatched(this._onObjectMatched, this);
-                                                // set data model on view
-                                                
-                                                var aFilters = [],
-                                                aFilters.push(new Filter("Soaid", FilterOperator.EQ, Value, "{/soaHeader/soaId}"));
-                                                var oList = this.getView().byId("SSPDetails");
-		                                     	var oBinding = oList.getBinding("items");
-	                                    		oBinding.filter(aFilter);
-                                                },
+		onInit: function() {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.getRoute("View2").attachPatternMatched(this._onObjectMatched, this);
+		},
 
-                                onExit: function() {
-                                                //Do Nothing
-                                },
-                                _onObjectMatched: function(oEvent) {
-                                                this.getView().bindElement({
-                                                                path: "/" + oEvent.getParameter("arguments").soaId,
-                                                                model: ""
-                                                });
-                                                var oData = {
-                                                                soaHeader: {
-                                                                                soaId: oEvent.getParameter("arguments").soaId
-                                                                }
-                                                };
-                                                var oModel = new JSONModel(oData);
-                                                this.getView().setModel(oModel);
-                                }
-
-                });
+		onExit: function() {
+			//Do Nothing
+		},
+		_onObjectMatched: function(oEvent) {
+			var oModel = this.getView().getModel();
+			var ssoaId = oEvent.getParameter("arguments").soaId;
+			var oKeyParams = {Soaid: ssoaId};
+			var sViewPath = oModel.createKey("/SOAHeaderDetailsSet", oKeyParams);
+			this.getView().bindElement({
+					path : sViewPath
+				});
+		}
+	});
 
 });
